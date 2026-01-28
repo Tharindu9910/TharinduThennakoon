36c75557306bec0fd184d4e33b29f47bcdadffe6
tharindu <tharindu@epitoni.com>
2026-01-28 23:46:34 +0530

    config file change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/tharinduthennakoon",
+  base:"/TharinduThennakoon",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
